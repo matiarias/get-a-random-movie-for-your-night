b34d984657a0c8e5e8876a98c5b283b31883699a
Matias Arias <matt.arias182@gmail.com>
2022-10-12 23:50:20 -0300

    instalando todas las dependencias para comenzar el proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Get a random movie for your night app
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-indigo-800 flex flex-col justify-center items-center gap-4">
+      <h1 className="text-red-600 text-4xl">Movies app</h1>
+    </div>
+  );
+}
+
+export default App;
