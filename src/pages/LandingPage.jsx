e21318fb6aaa6573581c0e71dc4696189c912759
Matias Arias <matt.arias182@gmail.com>
2022-10-13 02:15:51 -0300

    se agrego video background y botón iniciar a la page landing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import LoadingLanding from "../components/LoadingLanding/LoadingLanding";
+import videoLanding from "../assets/eterno-resplandor-de-una-mente-sin-recuerdos.mp4";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   const [loading, setLoading] = useState(true);
@@ -15,8 +17,22 @@ const LandingPage = () => {
       {loading ? (
         <LoadingLanding />
       ) : (
-        <div>
-          <h1 className="text-red-700 text-3xl">Landing</h1>
+        <div className="relative h-screen w-screen">
+          <video
+            className="object-cover h-full w-full"
+            src={videoLanding}
+            autoPlay
+            muted
+            loop
+          ></video>
+          <div className="absolute top-0 left-0 h-full w-full bg-gray-800/40"></div>
+          <div className="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center">
+            <Link to="/home">
+              <button className="px-20 py-2 text-white text-xl font-bold rounded-xl shadow-md shadow-gray-900 bg-gradient-to-r from-red-900 via-red-600 to-purple-900">
+                Iniciar
+              </button>
+            </Link>
+          </div>
         </div>
       )}
     </>
